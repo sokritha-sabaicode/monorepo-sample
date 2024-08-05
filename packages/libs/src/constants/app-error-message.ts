e6fb09a92a085f2e2e1d85fa7c14ddcfcb0a7d6e
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-08-05 13:29:41 +0700

    fix: update type and main in package.json mslib

--- packages/libs/src/constants/app-error-message.ts
+++ packages/libs/src/constants/app-error-message.ts
@@ -7,7 +7,7 @@ export const APP_ERROR_MESSAGE = {
   userDoesntExist: "User does not exist",
   invalidCredentials: "Invalid user email or password",
   invalidEmail: "Enter a valid email address",
-  existedEmail: "Email is already existed.", // WILL REMOVE LATER
+  existedEmail: "Email is already existed.", // REMOVE LATER
   existedAccount: "User already exists. Please use a different email or phone number.",
   verifyFail: "Your provided code is not correct. Please try again!"
 };
