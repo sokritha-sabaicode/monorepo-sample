c334e65fe68c5285a9bc25fb97d8b1f7f6e83d37
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-09-15 13:34:29 +0700

    fix: remove unnessary log

--- apps/backend/auth-service/src/server.ts
+++ apps/backend/auth-service/src/server.ts
@@ -5,7 +5,7 @@ import configs from "@/src/config";
 async function run() {
   try {
     app.listen(configs.port, () => {
-      console.log(`User Service running on Port:`, configs.port)
+      console.log(`Auth Service running on Port:`, configs.port)
     })
   } catch (error) {
     console.error("Failed to start the application:", error);
