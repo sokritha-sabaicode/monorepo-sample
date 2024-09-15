3a0684519fe348a1697b5a841f53f0297e1c73b0
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-09-15 15:11:23 +0700

    feat: add health route

---
+++ apps/backend/auth-service/src/controllers/health.controller.ts
@@ -0,0 +1,14 @@
+import sendResponse from "@/src/utils/send-response";
+import { Controller, Get, Route } from "tsoa";
+
+@Route('v1/auth')
+export class HealthController extends Controller {
+  @Get("/health")
+  public async getHealth(): Promise<{ message: string }> {
+    try {
+      return sendResponse({ message: 'OK' })
+    } catch (error) {
+      throw error;
+    }
+  }
+}
