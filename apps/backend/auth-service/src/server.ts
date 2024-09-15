3e41fd2ff1d620ef4cb38927e9dd97308cbd6b8d
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-09-15 08:46:15 +0700

    chore: add comments to trigger CI

--- apps/backend/auth-service/src/server.ts
+++ apps/backend/auth-service/src/server.ts
@@ -8,7 +8,7 @@ async function run() {
     })
   } catch (error) {
     console.error("Failed to start the application:", error);
-    process.exit(1); // Exit with failure code
+    process.exit(1); // Exit with failure code!
   }
 }
 
