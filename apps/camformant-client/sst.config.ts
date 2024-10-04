6031b5d9f452d5849b010bd0c3a4e0f9e2a4c228
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-10-04 22:32:20 +0700

    fix: add new env to camformant client

--- apps/camformant-client/sst.config.ts
+++ apps/camformant-client/sst.config.ts
@@ -13,8 +13,9 @@ export default {
       const site = new NextjsSite(stack, "site", {
         environment: {
           NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
-          NEXT_PUBLIC_AUTH_SERVICE_URL: process.env.NEXT_PUBLIC_AUTH_SERVICE_URL || '',
-          NEXT_PUBLIC_USER_SERVICE_URL: process.env.NEXT_PUBLIC_USER_SERVICE_URL || '',
+          NEXT_PUBLIC_AUTH_ENDPOINT: process.env.NEXT_PUBLIC_AUTH_ENDPOINT || '',
+          NEXT_PUBLIC_USER_ENDPOINT: process.env.NEXT_PUBLIC_USER_ENDPOINT || '',
+          NEXT_PUBLIC_JOB_ENDPOINT: process.env.NEXT_PUBLIC_JOB_ENDPOINT || '',
           NEXT_PUBLIC_PUSH_NOTIFICATION_SERVICE_URL: process.env.NEXT_PUBLIC_PUSH_NOTIFICATION_SERVICE_URL || '',
           NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '',
         }
