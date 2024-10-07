820f2f6271c09f15788024d8f939194bebaccc96
sokritha17@gmail.com <sokritha@sabaicode.com>
2024-10-07 20:26:05 +0700

    fix: redeploy camformant

--- apps/backend/job-service/src/app.ts
+++ apps/backend/job-service/src/app.ts
@@ -1,4 +1,3 @@
-import configs from '@/src/config';
 import { globalErrorHandler } from '@/src/middlewares/global-error';
 import loggerMiddleware from '@/src/middlewares/logger-middleware';
 import { RegisterRoutes } from '@/src/routes/v1/routes';
@@ -6,8 +5,6 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path'
 import swaggerUi from "swagger-ui-express";
-import cors from 'cors';
-
 
 // Dynamically load swagger.json & Initialize Sentry
 const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs/swagger.json'), 'utf8'));
@@ -18,12 +15,6 @@ const app = express()
 // ================================
 // Security Middleware
 // ================================
-const corsOptions = {
-  origin: configs.clientUrl,
-  credentials: true, // Request includes credentials like cookies
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-};
-app.use(cors(corsOptions));
 
 // ================================
 // Global Middleware
